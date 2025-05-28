79dbace896b7bdf008d259412e50d7a8bd7ec0c9
barsharanisethy <nexora37@gmail.com>
2025-05-28 12:59:06 +0530

    Add files via upload

--- python programming.py
+++ python programming.py
@@ -1,3 +1,4 @@
+
 #if esle statment
 marks=70
 if marks==100:
@@ -12,7 +13,7 @@ print("eat")
 h="apple"
 print("sweet") if h=="apple" or h=="banana" else "not sweet"
 age=56
- #vote=("yes","no")[age<18] not valid only in vs cocde 
+ #vote=("yes","no")[age<18] not valid only in vs cocde
  #string
 n="barshha"
 print(len(n))
@@ -20,7 +21,7 @@ t=" i am barsha"
 print(t.endswith('ha'))
 print(t.capitalize())
 print(t.replace("a" ,"i"))
-print(t.find("barsha")) 
+print(t.find("barsha"))
 #dictionary
 you= {
     "name" :"barsha",
@@ -30,3 +31,41 @@ you= {
 }
 print(you)
 print(list(you.keys()))
+#loop
+i=1
+while i<5:
+   print("heyy")
+   i=i+1
+j=1
+while j<=10:
+        print(3*j)
+        j=j+1
+        n=[4,8,2,1,6,7]
+        idx=0
+        while idx<=len(n)-1:
+            print(n[idx])
+            idx+=1
+            #for loop
+            l=[75,98,34,32]
+            for num in l:
+                print(num)
+            else:
+                print("end")
+                for e in range(1,5,2):
+                    pass
+                if e==3:
+                     print(e)
+                     #function in python
+                     def calculate (x,y):
+                        print(x*y)
+                     calculate(6,4)
+                     #recursion
+                     def show(n):
+                        if(n==0):#base case
+                             return
+                        print(n)
+                        show(n-1)
+                        print("end")
+
+        show(3)
+                    # this is a new line##this is second line# this is a new line
